3274d9294c2fc1a9ced93f8dfd71b5b81931ff8f
Fabricio la Torre <39594850+FabricioLatl@users.noreply.github.com>
2023-03-11 15:24:25 -0500

    only commit

---
+++ src/content_option.js
@@ -0,0 +1,144 @@
+const logotext = "Fabricio";
+const meta = {
+    title: "Fabricio La Torre",
+    description: "I'm Fabricio, a full stack mobile and web developer.",
+};
+
+const introdata = {
+    title: "I’m Fabricio La Torre",
+    animated: {
+        first: "I make beautiful designs",
+        second: "I make cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "Creativity, Innovation, Design",
+    your_img_url: "https://i.ibb.co/v3d1Nsx/2583048-3516.jpg",
+};
+
+const dataabout = {
+    title: "A little bit about myself",
+    aboutme: "I'm a Peruvian full stack developer, specialized in making both mobile applications and websites. I believe creativity is the soul of a project. And work is the tool to turn it into a reality.",
+};
+const worktimeline = [{
+        jobtitle: "Mobile App Developer",
+        where: "Asciende",
+        date: "Jan 2023 - Current",
+    },
+    
+];
+
+const educationtimeline = [{
+    jobtitle: "Bsc. Software Engineering",
+    where: "San Ignacio de Loyola University, Lima, Peru.",
+    date: "2024",
+},
+
+{
+    jobtitle: "CS50 Introduction to Web Development",
+    where: "EDX @ Harvard University",
+    date: "2019",
+},
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Firebase",
+        value: 85,
+    },
+    {
+        name: "MongoDB",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 80,
+    },
+    {
+        name: "React Native",
+        value: 70,
+    },
+    {
+        name: "Flutter",
+        value: 70,
+    },
+    {
+        name: "Adobe XD",
+        value: "80",
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "As a UI/UX designer, I can create visually stunning and user-friendly interfaces that enhance the user experience and increase engagement with your web and mobile applications.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "As a mobile app developer, I can create high-quality and engaging applications for iOS and Android devices using the latest technologies and best practices. With a focus on performance, usability, and functionality.",
+    },
+    {
+        title: "Web",
+        description: "As a React web developer, I can create modern and interactive web applications with a focus on performance and usability. With expertise in React, Redux, and other related libraries and tools, I strive to deliver high-quality and scalable applications that meet the needs of businesses and their users.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://i.ibb.co/Y8Mm3Tp/image.png",
+        desctiption: "URAMA crypto landing page, developed freelance using React.",
+        link: "https://fabriciolatl.github.io/",
+    },
+    {
+        img: "https://i.ibb.co/9vv91k9/image.png",
+        desctiption: "Google Calendar clone webapp, developed freelance with React and Firebase",
+        link: "https://github.com/FabricioLatl/Google-Calendar",
+    },
+    {
+        img: "https://i.ibb.co/PxdWj3Q/Whats-App-Image-2023-03-01-at-12-40-27.jpg",
+        desctiption: "Riqui dashboard. Created for Asciende in 2023. Utilizing React, Firebase and Tailwind (link pending)  ",
+        link: "#",
+    },
+    {
+        img: "https://i.ibb.co/mJtz8Ds/Whats-App-Image-2023-03-01-at-10-42-24.jpg",
+        desctiption: "Liqour delivery app. Developed for Asciende 2023 utilizing Flutter and Firebase (link pending)",
+        link: "#",
+    },
+    {
+        img: "  https://i.ibb.co/hWv32Rd/Whats-App-Image-2023-03-01-at-10-42-23.jpg",
+        desctiption: "Real Estate app. Developed for Asciende 2023 utilizing Flutter and Firebase (link pending)",
+        link: "#",
+    },
+  
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "fabriciolatorrel@gmail.com",
+    YOUR_FONE: "+51 982859000",
+    description: "Contact me and let's schedule a meeting for the next big project.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "fabriciolatorrel",
+    YOUR_TEMPLATE_ID: "template_nwrzs4u",
+    YOUR_USER_ID: "J9a3Duz6cjEbazxO5",
+};
+
+const socialprofils = {
+    github: "https://github.com/fabriciolatl",
+    linkedin: "https://linkedin.com/in/fabriciolatl",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    educationtimeline,  
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
